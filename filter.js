06ca2b199b87d153e185b96aa2653d9bd07b8d98
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-26 22:47:00 +0800

    added filter.js

---
+++ filter.js
@@ -0,0 +1,8 @@
+const bigestNum = [12, 20, 33, 23, 67, 88, 99, 44];
+const num = bigestNum.filter((num) => num < 30);
+
+const num2 = bigestNum.filter((num) => num > 50);
+const num3 = bigestNum.filter((num) => num > 100);
+// console.log(num2);
+console.log(num3);
+// console.log(num);
