3f6472a894e4b2ac093791cb35040ac65db41d48
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-26 21:18:21 +0800

    added chain data access

---
+++ chain.js
@@ -0,0 +1,25 @@
+// how to access data !
+
+// const personName = {
+//     name: 'babul',
+//     age: 22,
+//     address: {
+//         city: 'chittagong',
+//         area: 'amin koloni',
+//         study: [
+//             {id: 1, clg: 'textile institute', position: 'Textile engineer'},
+//             {id: 2, clg: 'textile vocational', position: 'basic class'}
+//         ]
+//     }
+// }
+
+// const result = personName.address.study[1].clg;
+// const result2 = personName.address.study[1].id;
+// console.log(result, result2);
+
+const friends = [
+    {name1: "jafor", age: 23, identy: 'student'},
+    {name1: "ullha", age: 40, identy: 'kamla'}
+]
+
+console.log(friends[1].identy);
